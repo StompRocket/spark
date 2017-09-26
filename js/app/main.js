47fb96faf231de1d8e61a8e92cef65018e4ea2e8
Rosho123 <rsf.sho@gmail.com>
2017-09-25 22:56:07 -0700

    auth

--- js/app/main.js
+++ js/app/main.js
@@ -81,6 +81,22 @@ var app = new Vue({
           global.chat()
         } else {
           document.title = 'Spark'
+          firebase.auth().signInWithPopup(provider).then(function (result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken
+  // The signed-in user info.
+            var user = result.user
+  // ...
+          }).catch(function (error) {
+  // Handle Errors here.
+            var errorCode = error.code
+            var errorMessage = error.message
+  // The email of the user's account used.
+            var email = error.email
+  // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential
+  // ...
+          })
         }
         var chatsRef = firebase.database().ref('users/' + uid)
         chatsRef.on('child_added', function (data) {
