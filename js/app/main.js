60ac2dc4d8663ffb99947b741fc997fa7fcd4ad5
Rosho123 <rsf.sho@gmail.com>
2017-11-03 19:14:09 -0700

    menu

--- js/app/main.js
+++ js/app/main.js
@@ -79,6 +79,18 @@ var app = new Vue({
   },
   mounted: function () {
     firebase.auth().onAuthStateChanged(function (user) {
+      $('.dropdown-button').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrainWidth: false, // Does not change width of dropdown to that of the activator
+    hover: true, // Activate on hover
+    gutter: 0, // Spacing from edge
+    belowOrigin: false, // Displays dropdown below the button
+    alignment: 'left', // Displays dropdown with edge aligned to the left of button
+    stopPropagation: false // Stops event propagation
+  }
+);
+    
       if (user) {
         app.loginText = 'Logout'
         var user = firebase.auth().currentUser
